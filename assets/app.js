bc883793d4021a69907f6f71bece7b5345c5760c
Andretreccia <andreabtreccia@gmail.com>
2021-10-29 15:16:31 +0200

    Aggiunte immagini stramite vujs

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,36 @@
 const app = new Vue({
     el: '#root',
+    data: {
+        images: [
+            {
+                items: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                items: 'img/02.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum',
+            },
+            {
+                items: 'img/03.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                items: 'img/04.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam',
+            },
+            {
+                items: 'img/05.jpg',
+                title: 'Svezia',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            },
+
+
+        ],
+
+    },
 
 })
